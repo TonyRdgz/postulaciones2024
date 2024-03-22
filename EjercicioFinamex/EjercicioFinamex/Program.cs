e0fa5be56fdbdd7aa90302743db4adc99d64c5f5
Tony Rockdriguez <ars.proy@gmail.com>
2024-03-21 22:28:43 -0600

    First commit-Multiplos 3 y 5
    Ejercicio de evaluación de Finamex- 100 enteros

---
+++ EjercicioFinamex/EjercicioFinamex/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EjercicioFinamex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se declara un ciclo de enteros hasta 100
+            for (int num=0; num <= 100; num++) {
+
+                //Se declara la primera condición de nuestro programa, para cuando si el entero "num" es multiplo de 3 o 5, imprima: "FizzBuzz"
+                if (num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+
+                }
+                //Se declara la tercera condición, para cuando si el entero "num" es multiplo de 3, imprima: "Fizz"
+                else if (num % 3 == 0) {
+                    Console.WriteLine("Fizz");
+
+                }
+                //Se declara la segunda condición, para cuando si el entero "num" es multiplo de 5, imprima: "Buzz"
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                //Se declara la cuarta condición o default, para cuando si el entero "num" NO es multiplo de 3 ni 5, imprima: num
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+
+        }
+    }
+}
